d9f0a114e94715217255c61d3e1f451ceb96e652
Girma Nigusse <xgirma@gmail.com>
2020-08-11 23:34:36 -0400

    updates

---
+++ src/referrers_doc.js
@@ -0,0 +1,57 @@
+import { writeFileSync } from "fs";
+import table from "markdown-table";
+import { logger } from "./constants";
+import { actionsStatusHome, bubbleSort } from "./utils";
+
+function getCount(stat) {
+    let count = 0;
+    stat.forEach((c) => {
+        count += c.count;
+    });
+    return count;
+}
+
+function getUniques(temp) {
+    let count = 0;
+    temp.forEach((t) => {
+        count += t.uniques;
+    });
+    return count;
+}
+
+function getReferrers(stat) {
+    const refs = [];
+    stat.sort();
+    stat.forEach((ref) => {
+        refs.push(ref.referrer);
+    });
+    return refs.join(", ");
+}
+
+function writeReferrers() {
+    try {
+        const redacted = require(`../data/redacted/redacted.json`);
+        const clone = [];
+
+        redacted.forEach((r) => {
+            const row = [];
+            const data = r.referrers;
+            row.push(getCount(data));
+            row.push(getUniques(data));
+            row.push(getReferrers(data));
+            row.push(actionsStatusHome(r.name));
+            clone.push(row);
+        });
+
+        const content = bubbleSort(clone);
+        content.unshift(["Count", "Unique", "Referrers", "Repository"]);
+
+        const path = "docs/referrers.md";
+        writeFileSync(path, table(content, { align: "l" }), "utf8");
+        logger.info(`Write ${path}`);
+    } catch (error) {
+        logger.error(error.message);
+    }
+}
+
+writeReferrers();
