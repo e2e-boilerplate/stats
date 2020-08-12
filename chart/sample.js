d5b51a73a6afdb1043bac3bf06b3f11bc6d35f54
Girma Nigusse <xgirma@gmail.com>
2020-08-12 19:33:09 -0400

    updates

--- chart/sample.js
+++ chart/sample.js
@@ -1,6 +1,19 @@
 import { writeFileSync } from "fs";
 import { logger } from "../src/constants";
 
+function prepareSample(obj) {
+  const sample = [];
+  const names = Object.getOwnPropertyNames(obj);
+  names.forEach( name => {
+    sample.push({
+      "name": name,
+      "value": obj[name]
+    });
+  });
+
+  return sample;
+}
+
 function getSample() {
   try {
     const redacted = require(`../data/redacted/redacted.json`);
@@ -22,8 +35,11 @@ function getSample() {
       });
     });
 
-    const contentCount = JSON.stringify(countData);
-    const contentUnique = JSON.stringify(uniqueData);
+    const formattedCount = prepareSample(countData);
+    const formattedUnique = prepareSample(uniqueData);
+
+    const contentCount = JSON.stringify(formattedCount,null, 2);
+    const contentUnique = JSON.stringify(formattedUnique,null, 2);
 
     writeFileSync("data/chart/referrer/count.json", contentCount, "utf8");
     writeFileSync("data/chart/referrer/unique.json", contentUnique, "utf8");
