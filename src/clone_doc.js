a72ba2456bda6711a7985d3297cb70e68d27410d
Girma Nigusse <xgirma@gmail.com>
2020-08-11 22:37:45 -0400

    updates

--- src/clone_doc.js
+++ src/clone_doc.js
@@ -1,31 +1,31 @@
-import { readdirSync, readFileSync, writeFileSync } from "fs";
+import { writeFileSync } from "fs";
 import table from "markdown-table";
-import { logger, options, user } from "./constants";
+import { logger } from "./constants";
 import { actionsStatusHome, bubbleSort } from "./utils";
 
 function writeClone() {
-    try {
-        const redacted = require(`../data/redacted/redacted.json`);
-        const clone = [];
+  try {
+    const redacted = require(`../data/redacted/redacted.json`);
+    const clone = [];
 
-        redacted.forEach(r => {
-           const row = [];
-           const { count, uniques } = r.clones;
-           row.push(count);
-           row.push(uniques);
-           row.push(actionsStatusHome(r.name));
-           clone.push(row);
-        });
+    redacted.forEach((r) => {
+      const row = [];
+      const { count, uniques } = r.clones;
+      row.push(count);
+      row.push(uniques);
+      row.push(actionsStatusHome(r.name));
+      clone.push(row);
+    });
 
-        const content = bubbleSort(clone);
-        content.unshift(["Clone counts", "Unique clones", "repository"]);
+    const content = bubbleSort(clone);
+    content.unshift(["Clone counts", "Unique clones", "repository"]);
 
-        const path = 'docs/clone.md';
-        writeFileSync(path, table(content, { align: "l" }), "utf8");
-        logger.info(`Write ${path}`);
-    } catch(error) {
-        logger.error(error.message);
-    }
+    const path = "docs/clone.md";
+    writeFileSync(path, table(content, { align: "l" }), "utf8");
+    logger.info(`Write ${path}`);
+  } catch (error) {
+    logger.error(error.message);
+  }
 }
 
 writeClone();
