3339a46139cd6798108b2487f6271d38a21c039c
Girma Nigusse <xgirma@gmail.com>
2020-08-14 01:57:17 +0000

    update stat

--- chart/bar_chart.js
+++ chart/bar_chart.js
@@ -19,33 +19,33 @@ function barChartSvg(data, path) {
   const x = d3.scaleLinear().range([0, width]);
 
   const svg = body
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .attr("xmlns", "http://www.w3.org/2000/svg")
-      .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
-      .append("g")
-      .attr("transform", `translate(${margin.left},${margin.top})`);
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("xmlns", "http://www.w3.org/2000/svg")
+    .attr("xmlns:xlink", "http://www.w3.org/1999/xlink")
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
   x.domain([0, d3.max(data, (d) => d.value)]);
   y.domain(data.map((d) => d.name));
 
   svg
-      .selectAll(".bar")
-      .data(data)
-      .enter()
-      .append("rect")
-      .attr("class", "bar")
-      .attr("width", (d) => x(d.value))
-      .attr("y", (d) => y(d.name))
-      .attr("height", y.bandwidth())
-      .style("fill", "#f4c63d")
-      .style("text-anchor", "end");
+    .selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("width", (d) => x(d.value))
+    .attr("y", (d) => y(d.name))
+    .attr("height", y.bandwidth())
+    .style("fill", "#f4c63d")
+    .style("text-anchor", "end");
 
   svg
-      .append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(x));
+    .append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(x));
 
   svg.append("g").call(d3.axisLeft(y));
 
