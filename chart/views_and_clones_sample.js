e6fff2772d135c7ff1e16d48b371b32d3773abe6
Girma Nigusse <xgirma@gmail.com>
2020-08-13 23:31:04 -0400

    updates

--- chart/views_and_clones_sample.js
+++ chart/views_and_clones_sample.js
@@ -2,17 +2,16 @@ import { logger } from "../src/constants";
 
 const { appendFileSync, writeFileSync } = require("fs");
 
-function writeToFile(header, data) {
+function writeToFile(data, path) {
   const content = ["name,count,uniques"];
-  const file = "chart/views/views.csv";
 
   data.forEach((element) => content.push(element));
 
-  writeFileSync(file, "", "utf8");
+  writeFileSync(path, "", "utf8");
 
   try {
     content.forEach((line) => {
-      appendFileSync(file, `${line}\n`, "utf8");
+      appendFileSync(path, `${line}\n`, "utf8");
     });
   } catch (error) {
     console.error("Error writing csv to file");
@@ -22,19 +21,27 @@ function writeToFile(header, data) {
 function createCSv() {
   try {
     const redacted = require(`../data/redacted/redacted.json`);
-    const data = [];
+    const view = [];
+    const clone = [];
 
     redacted.forEach((r) => {
       const { views, name } = r;
       const { count, uniques } = views;
-      if (count === 0 && uniques === 0) {
-        data.push(`${name},${count},${uniques}`);
+      if (count !== 0 && uniques !== 0) {
+          view.push(`${name},${count},${uniques}`);
       }
     });
 
-    console.log(data);
+      redacted.forEach((r) => {
+          const { clones, name } = r;
+          const { count, uniques } = clones;
+          if (count !== 0 && uniques !== 0) {
+              clone.push(`${name},${count},${uniques}`);
+          }
+      });
 
-    writeToFile();
+    writeToFile(view, "chart/views/views.csv");
+    writeToFile(clone, "chart/clones/clones.csv");
   } catch (error) {
     logger.error(error.message);
   }
